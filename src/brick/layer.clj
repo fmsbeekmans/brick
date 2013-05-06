718df7a7cf64e2cd38f8cd7ecd2c507360950b7d
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2013-05-06 17:16:25 +0200

    grid layer prototype

--- src/brick/layer.clj
+++ src/brick/layer.clj
@@ -1,11 +1,27 @@
-(ns brick.layer)
+(ns brick.layer
+  (:use quil.core))
 
 (defprotocol Layer
   (draw [_]))
 
-(defrecord GridLayer [:h :w :tiles]
+(defrecord GridLayer [h w tiles]
   Layer
   (draw [this]
-    (doall
-     (for [x (range (:w this))
-           y (range (:h this))]))))
+    (let [tile-h (/ (height) (:h this))
+          tile-w (/ (width) (:w this))]
+      (doall
+       (for [x (range (:w this))
+             y (range (:h this))]
+         (with-translation [(* x tile-w)
+                            (* y tile-h)]
+           (((:tiles this) [x y]) tile-w tile-h)))))))
+
+(defn init-grid-layer [h w init-fn]
+  (GridLayer. h w
+              (apply hash-map
+                     (mapcat
+                      (fn [[[x y] v]]
+                        [[x y] v])
+                      (doall (for [x (range w)
+                                   y (range h)]
+                               [[x y] (init-fn x y)]))))))
