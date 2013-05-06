89bedd0a6f1dea4e70c1c0dd363a323169d42079
jlicht <jlicht@aerix.nl>
2013-05-06 17:49:39 +0200

    Created a small HUD-debugger for quil applications

--- src/brick/core.clj
+++ src/brick/core.clj
@@ -1,17 +1,22 @@
 (ns brick.core
   (:use [quil.core])
-  (:require [brick.tile :as tile])
+  (:require [brick.tile :as tile]
+            [brick.debug :as debug])
   (:gen-class))
 
 (defn setup []
   (smooth)
   (frame-rate 1)
   (background 255)
+  (def dbg ( debug/fps-dbg))
+  (debug/toggle dbg)
   (def tiles
     ((tile/load-tiles (load-image "resources/tiles2.png") 32)
      {:bricks 1
       :grass 2
-      :bush 4})))
+      :bush 4
+      :shroomr 11
+      :shrooml 9})))
 
 (defn draw-grid []
   (doall
@@ -19,12 +24,15 @@
          y (range 10)]
      (image (tiles
              (if (odd? (+ x y))
-               :grass
-               :bricks)) (* x 32) (* y 32))))
-  (with-translation [10 10] (image (tiles :bush) (* 32 (rand-int 10)) (* 32 (rand-int 10)))))
+               :shrooml
+               :shroomr)) (* x 32) (* y 32))))
+  (with-translation [10 10] (image (tiles :bush) (* 32 (rand-int 10)) (* 32 (rand-int 10))))
+  ;(debug/draw dbg)
+  )
+
 
 (defn draw-layer
-)
+  )
 
 (defn start []
   (defsketch example
@@ -33,4 +41,4 @@
     :draw (var draw-grid)))
 
 (defn -main []
-)
+  )
