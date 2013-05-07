1acdb7a8ac7a9926714db71afccf3ce25e7fbf50
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2013-05-07 17:59:54 +0200

    nested dictionary.

--- src/brick/core.clj
+++ src/brick/core.clj
@@ -17,12 +17,14 @@
 
 (defn draw [tiles-atom]
   (tile/with-tiles (vec @tiles-atom)
-      (tile/with-dictionary {:a 0
+    (tile/with-dictionary {:paths {:road 0
+                                   :canal 3
+                                   :railroad 5}
                              :b 1
                              :c 2}
         (with-translation [100 100]
           (background 0)
-          ((tile/tile :b) 20 20)))))
+          ((tile/tile :paths :canal) 20 20)))))
 
 
 (defn start []
