80f86f4a175682400ad97e355bb5c4d297a426d6
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2013-05-07 18:35:23 +0000

    brick function to start the engine.

--- src/brick/core.clj
+++ src/brick/core.clj
@@ -5,6 +5,22 @@
             [brick.debug :as debug])
   (:import [brick.layer GridLayer]))
 
+(declare setup)
+(declare draw)
+
+
+(def t (atom 0))
+
+(defn start []
+  (brick "Tyler" [640 320] #() (atom []) nil))
+
+(defn brick [title size tile-init layers tile-atom command-que]
+  (defsketch example
+    :title title
+    :setup  (fn [] (setup tiles layers))
+    :draw ((var draw) tiles layers)
+    :size size))
+
 (defn setup [tiles-atom]
     (swap! tiles-atom
          (fn [tiles]
@@ -13,24 +29,14 @@
                     (load-image "resources/tiles2.png") 32))))
     (smooth))
 
-(def t (atom 0))
-
-(defn draw [tiles-atom]
+(defn draw [tiles-atom layers]
   (tile/with-tiles (vec @tiles-atom)
     (tile/with-dictionary {:paths {:road 0
                                    :canal 3
                                    :railroad 5}
                              :b 1
                              :c 2}
+      
         (with-translation [100 100]
           (background 0)
           ((tile/tile :paths :canal) 20 20)))))
-
-
-(defn start []
-  (let [tiles (atom [])]
-    (defsketch example
-      :title "Tiles!"
-      :setup #(setup tiles)
-      :draw #((var draw) tiles)
-      :size [320 320])))
