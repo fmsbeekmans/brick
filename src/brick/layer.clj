e069bde51615f4bdefecc75704c9f92b151287b1
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2013-05-07 18:35:49 +0000

    Cache layers?

--- src/brick/layer.clj
+++ src/brick/layer.clj
@@ -2,19 +2,27 @@
   (:use quil.core))
 
 (defprotocol Layer
-  (draw [_]))
+  (draw [_ _]))
 
-(defrecord GridLayer [h w tiles]
+(defrecord GridLayer [h w tiles cache?]
   Layer
-  (draw [this]
-    (let [tile-h (/ (height) (:h this))
-          tile-w (/ (width) (:w this))]
-      (doall
-       (for [x (range (:w this))
-             y (range (:h this))]
-         (with-translation [(* x tile-w)
-                            (* y tile-h)]
-           (((:tiles this) [x y]) tile-w tile-h)))))))
+  (draw [this cache]
+    (if (cache? this)
+      (do
+        (let [tile-h (/ (height) (:h this))
+              tile-w (/ (width) (:w this))]
+          (doall
+           (for [x (range (:w this))
+                 y (range (:h this))]
+             (with-translation [(* x tile-w)
+                                (* y tile-h)]
+               (((:tiles this) [x y]) tile-w tile-h)))))
+        (do
+          ;draw from cache
+          )))
+    (cache [this]
+           ;save rendering of this layer alone.
+           )))
 
 (defn init-grid-layer [h w init-fn]
   (GridLayer. h w
@@ -24,4 +32,5 @@
                         [[x y] v])
                       (doall (for [x (range w)
                                    y (range h)]
-                               [[x y] (init-fn x y)]))))))
+                               [[x y] (init-fn x y)]))))
+              #(false)))
