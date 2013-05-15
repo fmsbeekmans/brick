4e667a47bbfe55c1db043346f4fa5c6ed207af95
jlicht <jlicht@aerix.nl>
2013-05-15 15:23:23 +0200

    Changed the example to conform and work with the new code

--- src/brick/examples/drawable.clj
+++ src/brick/examples/drawable.clj
@@ -1,28 +1,43 @@
 (ns brick.examples.drawable
   (:use [quil.core :exclude [size]]
-        brick.core
-        brick.drawable
-        brick.image)
+        brick.core)
+  (:require [brick.drawable :as drawable]
+            [brick.image :as image])
   (:gen-class))
 
-(def br (defbricklet
-          (atom [])
-          :layers-init
-          (fn [bricklet]
-            (swap! (:layers bricklet)
-                   (fn [old]
-                     [(->Grid 3 1 {[0 0] (->Image (load-image "colors.png"))
-                                   [1 0] (->Stack [(get-image-in bricklet :bush-l)
-                                                   (->Grid 1 2 {[0 0] (get-image-in bricklet 0)
-                                                                [0 1] (get-image-in bricklet 7)})])})])))
-          :execute-queue (atom [])
-          :dictionary {:bricks 1
-                       :bush-l 4
-                       :bush-r 5}
-          :images (atom [])
-          :size [500 500]))
+;;; These Vars should be abstracted by let bindings in production usage
+(def images (atom []))
+(def layers (atom []))
+(def commands (atom []))
+(def dict {:bricks 1 :bush-l 4 :bush-r 5})
 
-(brick-sketch a br)
+(defn- images-init [old]
+  (vec (concat old
+               (image/load-images (load-image "resources/32x32.png") [32 32]))))
 
-(schedule br (fn []
-              (background 50 50 100)))
+(defn- layers-init [old]
+  (let [lookup #(@images (image/dictionary dict %))]
+    [(drawable/->Grid 2 1 {[0 0] (drawable/->Image (load-image "colors.png"))
+                  [1 0]
+                  (drawable/->Stack [(lookup :bush-l)
+                            (drawable/->Grid 1 2 {[0 0] (lookup 0)
+                                         [0 1] (lookup 7)})])})]))
+
+(defn- setup
+  "Prepare a bricklet. This includes initializing tiles and layers."
+  [bricklet]
+  (frame-rate 2)
+  (swap! images images-init)
+  (swap! (:layers bricklet) layers-init))
+
+
+
+
+(def br (drawable/->Bricklet layers commands))
+
+(def br-sketch (bricklet-sketch br
+                                :setup #(setup br)
+                                :size [500 500]
+                                :title "Let there be title!"))
+(defn color-bg [bricklet]
+  (swap! (:command-queue bricklet) conj (fn [_] (background 50 50 100))))
