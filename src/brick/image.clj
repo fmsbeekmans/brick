b0c1d5bf8a2723716777ffbe493bda59c6346707
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2013-06-14 22:51:19 +0000

    Image bikeshed.

--- src/brick/image.clj
+++ src/brick/image.clj
@@ -5,7 +5,8 @@
   (:import [brick.drawable Image]))
 
 (defn load-images
-  "Cut a source image up into pieces of tile-w by tile-h and put them in a vector."
+  "Cut a source image up into pieces of tile-w by tile-h and put
+them in a vector."
   [source-image [tile-w tile-h]]
   (let [n-tiles (/ (.width source-image) tile-w)
         indexed-tiles
@@ -26,6 +27,8 @@
  [path]
  (in-draw-context (load-image path)))
 
-;;TODO resource-image should return a record containing a dictionary key, rather than a PImage. This makes the record comparable and comparable is good.
+;;TODO resource-image should return a record containing a dictionary
+;;key, rather than a PImage. This makes the record comparable and
+;;comparable is good.
 (defn resource-image [path]
   (draw/->Image (path->PImage (clojure.java.io/resource path))))
